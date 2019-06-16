4ff286738c14a78a5f4d9c8e4dbc0610ee11a465
Sergey Golovin <sergeygolovin@mac-mini-sergey.local>
2019-06-16 10:12:07 +0700

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Sergey Golovin on 16.06.2019.
+//  Copyright © 2019 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
