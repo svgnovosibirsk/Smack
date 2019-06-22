df192efdd491317c6a6a7c0adde4b389fca89371
Sergey Golovin <sergeygolovin@Mac-mini-Sergey.local>
2019-06-22 14:59:05 +0700

    Lesson4

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Sergey Golovin on 22.06.2019.
+//  Copyright © 2019 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Sergey Golovin on 22.06.2019.
+//  Copyright © 2019 GoldenWindGames LLC. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
