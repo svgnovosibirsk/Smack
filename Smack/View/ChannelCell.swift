f7295617208dcf3cfbea7920acf53b29d5d532ed
Sergey Golovin <sergeygolovin@Mac-mini-Sergey.local>
2019-07-15 18:58:51 +0700

    Lesson 21

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Sergey Golovin on 15.07.2019.
+//  Copyright © 2019 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+    
+}
